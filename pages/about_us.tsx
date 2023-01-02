934183083924c166f7396b767027d5faff89be16
GlennOnyango <glenntedd@gmail.com>
2023-01-02 16:53:44 +0300

    events app next js

---
+++ pages/about_us.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css'
+import Header from '../components/header'
+
+export default function AboutUs() {
+  return (
+    <>
+    <Header/>
+      <main className={styles.main}>
+        <h1>About Us</h1>
+      </main>
+    </>
+  )
+}
---
+++ pages/events/index.tsx
@@ -0,0 +1,61 @@
+import Header from '../../components/header'
+import styles from '../../styles/Home.module.css'
+
+export default function Events() {
+  return (
+    <>
+    <Header/>
+      <main className={styles.main}>
+        <h1>Events</h1>
+
+        <a href="">
+          <img/>
+          <h2>Events in London</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            mattis pellentesque diam, a vehicula arcu. Praesent consequat tellus
+            sed lorem vulputate accumsan. Morbi et viverra odio. Proin interdum
+            lectus id nunc lacinia, at gravida tellus suscipit. Donec congue
+            semper lacus pellentesque posuere. Pellentesque placerat viverra
+            augue, at euismod neque pulvinar in. Nam mattis lectus vel magna
+            aliquet dictum. Quisque cursus tempus tempus. Aliquam ultrices
+            ligula eget augue porttitor, blandit iaculis nulla auctor
+          </p>
+        </a>
+
+        <a href="">
+          <img/>
+          <h2>Events in London</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            mattis pellentesque diam, a vehicula arcu. Praesent consequat tellus
+            sed lorem vulputate accumsan. Morbi et viverra odio. Proin interdum
+            lectus id nunc lacinia, at gravida tellus suscipit. Donec congue
+            semper lacus pellentesque posuere. Pellentesque placerat viverra
+            augue, at euismod neque pulvinar in. Nam mattis lectus vel magna
+            aliquet dictum. Quisque cursus tempus tempus. Aliquam ultrices
+            ligula eget augue porttitor, blandit iaculis nulla auctor
+          </p>
+        </a>
+
+        <a href="">
+          <img/>
+          <h2>Events in London</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            mattis pellentesque diam, a vehicula arcu. Praesent consequat tellus
+            sed lorem vulputate accumsan. Morbi et viverra odio. Proin interdum
+            lectus id nunc lacinia, at gravida tellus suscipit. Donec congue
+            semper lacus pellentesque posuere. Pellentesque placerat viverra
+            augue, at euismod neque pulvinar in. Nam mattis lectus vel magna
+            aliquet dictum. Quisque cursus tempus tempus. Aliquam ultrices
+            ligula eget augue porttitor, blandit iaculis nulla auctor
+          </p>
+        </a>
+        
+
+
+      </main>
+    </>
+  )
+}
